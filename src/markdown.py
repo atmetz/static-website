d550c00f8b393a72a951b4cfe99ec23a67bc0195
atmetz <atmetz@gmail.com>
2025-06-02 23:00:03 -0500

    start split_nodes_image

--- src/markdown.py
+++ src/markdown.py
@@ -7,15 +7,19 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
-        else:
-            new_node = old_node.text.split(delimiter)
-            if len(new_node) != 3:
-                raise Exception(f"No closing delimiter found: {delimiter}")
-            new_node1 = [TextNode(f"{new_node[0]}", TextType.TEXT),
-                         TextNode(f"{new_node[1]}", text_type),
-                         TextNode(f"{new_node[2]}", TextType.TEXT)]
-            new_nodes.extend(new_node1)
-
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError(f"No closing delimiter found: {delimiter}")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
 
     return new_nodes
 
@@ -26,3 +30,6 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def split_nodes_image(old_nodes):
+    
